15e88f3de96a03ed083478984544dd31588d2eb4
minluna <znddiqjwjs@gmail.com>
2024-04-23 14:57:24 +0900

    :sparkles: Feat : Search 관련 API 구현

---
+++ src/main/java/com/example/java_spring/controller/SearchController.java
@@ -0,0 +1,38 @@
+package com.example.java_spring.controller;
+
+import com.example.java_spring.controller.dto.jwt.JwtUser;
+import com.example.java_spring.controller.dto.Result;
+import com.example.java_spring.service.SearchService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/search")
+public class SearchController {
+    private SearchService searchService;
+
+    public SearchController(SearchService searchService) {
+        this.searchService = searchService;
+    }
+
+    // 키워드로 게시물 불러오기
+    @GetMapping("/keyword")
+    public ResponseEntity<Object> findKeywordPost(Authentication authentication, @RequestParam String keyword, Integer cursor){
+        List<Map<String, Object>> searchPost = searchService.getKetwordPost(((JwtUser) authentication.getPrincipal()).getUserId(), keyword, cursor);
+
+        // JSON 응답 객체 생성
+        // ResponseEntity로 응답 반환
+        return ResponseEntity.ok(
+                Result.res(HttpStatus.OK.toString(),
+                        "키워드를 포함한 게시물 불러오기에 성공했습니다.",
+                        searchPost));
+    }
+}
---
+++ src/main/java/com/example/java_spring/service/SearchService.java
@@ -0,0 +1,88 @@
+package com.example.java_spring.service;
+
+import com.example.java_spring.exception.UnauthorizedException;
+import com.example.java_spring.jpa.repository.PostRepository;
+import com.example.java_spring.jpa.repository.UserRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class SearchService {
+    private UserRepository userRepository;
+    private PostRepository postRepository;
+
+    public SearchService(UserRepository userRepository, PostRepository postRepository) {
+        this.userRepository = userRepository;
+        this.postRepository = postRepository;
+    }
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    // 키워드로 게시물 불러오기
+    @Transactional
+    public List<Map<String, Object>> getKetwordPost(Integer userId, String keyword, Integer cursor) {
+        boolean existUser = userRepository.existsByIdAndDeletedAtIsNull(userId);
+
+        if (!existUser) {
+            throw new UnauthorizedException("잘못된 또는 만료된 토큰입니다.");
+        }
+
+        List<Object[]> results = new ArrayList<>();
+        if (cursor == 0) {
+            results = entityManager.createQuery(
+                            "SELECT p.id as postId, p.user_id as userId, u.nickname as nickname, p.content as content, pi.image_url as postImageUrl " +
+                                    "FROM Post p " +
+                                    "LEFT JOIN p.postImages pi " +
+                                    "LEFT JOIN p.user u " +
+                                    "WHERE p.deletedAt IS NULL AND u.deletedAt IS NULL AND p.content LIKE :keyword " +
+                                    "ORDER BY p.createdAt DESC"
+                    )
+                    .setParameter("keyword", "%" + keyword + "%")
+                    .setMaxResults(5)
+                    .getResultList();
+        }
+        else if (cursor == -1) {
+            results.add(new Object[]{"검색어가 포함된 게시물 조회가 끝났습니다."});
+        } else {
+            results = entityManager.createQuery(
+                            "SELECT p.id as postId, p.user_id as userId, u.nickname as nickname, p.content as content, pi.image_url as postImageUrl " +
+                                    "FROM Post p " +
+                                    "LEFT JOIN p.postImages pi " +
+                                    "LEFT JOIN p.user u " +
+                                    "WHERE p.deletedAt IS NULL AND u.deletedAt IS NULL AND p.content LIKE :keyword AND p.id < :cursor " +
+                                    "ORDER BY p.createdAt DESC"
+                    )
+                    .setParameter("keyword", "%" + keyword + "%")
+                    .setParameter("cursor", cursor)
+                    .setMaxResults(5)
+                    .getResultList();
+        }
+
+        List<Map<String, Object>> objectList = new ArrayList<>();
+        if (cursor == -1) {
+            Map<String, Object> endMessage = new HashMap<>();
+            endMessage.put("endMessage", results.get(0));
+            objectList.add(endMessage);
+        } else {
+            for (Object[] result : results) {
+                Map<String, Object> objectMap = new HashMap<>();
+                objectMap.put("postId", result[0]);
+                objectMap.put("userId", result[1]);
+                objectMap.put("nickname", result[2]);
+                objectMap.put("content", result[3]);
+                objectMap.put("postImage", result[4]);
+                // 다른 필드들에 대한 매핑 계속 추가
+                objectList.add(objectMap);
+            }
+        }
+        return objectList;
+    }
+}
